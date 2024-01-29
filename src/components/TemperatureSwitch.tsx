f0e7c963d7f023d89b1e6e14bc0520d620e4ef74
KaanOzen-mF <kaanozn98@gmail.com>
2024-01-29 15:20:23 +0300

    Temparature switch component UI done

---
+++ src/components/TemperatureSwitch.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function TemperatureSwitch() {
+  const [isCelsius, setIsCelsius] = useState(true); // Initialize to Celsius
+
+  const toggleTemperature = () => {
+    setIsCelsius(!isCelsius); // Toggle the temperature unit
+  };
+
+  console.log(isCelsius);
+
+  return (
+    <div className="switch-container" onClick={toggleTemperature}>
+      <div
+        className={`switch ${
+          isCelsius ? "celsius-active" : "fahrenheit-active"
+        }`}
+      >
+        <span className="temperature-label celsius">C°</span>
+        <span className="temperature-label fahrenheit">F°</span>
+        <div className="switch-circle"></div>
+      </div>
+    </div>
+  );
+}
