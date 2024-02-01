9ef946b95cdcde4baaca3336682b54bff9a042fb
KaanOzen-mF <kaanozn98@gmail.com>
2024-02-01 12:09:28 +0300

    weather info card done

--- src/components/WeatherInfoCard.tsx
+++ src/components/WeatherInfoCard.tsx
@@ -1,68 +1,77 @@
 import { FaCloud } from "react-icons/fa";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
+interface WeatherDataProps {
+  location: {
+    name: string;
+    country: string;
+  };
+  current: {
+    temp_c: number;
+    humidity: number;
+    gust_kph: number;
+  };
+  forecast: {
+    forecastday: [
+      {
+        hour: [
+          {
+            temp_c: number;
+            time: string;
+            condition: {
+              icon: string;
+            };
+          }
+        ];
+      }
+    ];
+  };
+}
 export default function WeatherInfoCard() {
+  const [weatherData, setWeatherData] = useState<WeatherDataProps | null>(null);
+
+  const fetchCurrentWeather = async (query: string) => {
+    const options = {
+      method: "GET",
+      url: "https://weatherapi-com.p.rapidapi.com/forecast.json",
+      params: { query },
+      headers: {
+        "X-RapidAPI-Key": "b12886698dmsh2a39c211cc8b4aep12a465jsn115794388fb5",
+        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+      Promise.all([fetchCurrentWeather(`${latitude},${longitude}`)]).then(
+        ([currentWeather]) => {
+          setWeatherData(currentWeather);
+        }
+      );
+    });
+  }, []);
+
   return (
     <div className="weatherHourlyCardContainer">
-      <div className="weatherHourlyCard">
-        <p className="weatherCardText">00 am</p>
-        <FaCloud size={32} />
-        <p className="weatherCardText">0°</p>
-      </div>
-      <div className="weatherHourlyCard">
-        <p className="weatherCardText">01 am</p>
-        <FaCloud size={32} />
-        <p className="weatherCardText">0°</p>
-      </div>
-      <div className="weatherHourlyCard">
-        <p className="weatherCardText">02 am</p>
-        <FaCloud size={32} />
-        <p className="weatherCardText">0°</p>
-      </div>
-      <div className="weatherHourlyCard">
-        <p className="weatherCardText">03 am</p>
-        <FaCloud size={32} />
-        <p className="weatherCardText">0°</p>
-      </div>
-      <div className="weatherHourlyCard">
-        <p className="weatherCardText">04 am</p>
-        <FaCloud size={32} />
-        <p>0°</p>
-      </div>
-      <div className="weatherHourlyCard">
-        <p className="weatherCardText">05 am</p>
-        <FaCloud size={32} />
-        <p>0°</p>
-      </div>
-      <div className="weatherHourlyCard">
-        <p className="weatherCardText">06 am</p>
-        <FaCloud size={32} />
-        <p>0°</p>
-      </div>
-      <div className="weatherHourlyCard">
-        <p className="weatherCardText">07 am</p>
-        <FaCloud size={32} />
-        <p>0°</p>
-      </div>
-      <div className="weatherHourlyCard">
-        <p className="weatherCardText">08 am</p>
-        <FaCloud size={32} />
-        <p>0°</p>
-      </div>
-      <div className="weatherHourlyCard">
-        <p className="weatherCardText">09 am</p>
-        <FaCloud size={32} />
-        <p>0°</p>
-      </div>
-      <div className="weatherHourlyCard">
-        <p className="weatherCardText">09 am</p>
-        <FaCloud size={32} />
-        <p>0°</p>
-      </div>
-      <div className="weatherHourlyCard">
-        <p className="weatherCardText">09 am</p>
-        <FaCloud size={32} />
-        <p>0°</p>
-      </div>
+      {weatherData?.forecast.forecastday[0].hour.map((i) => (
+        <div className="weatherHourlyCard">
+          <p className="weatherCardText">
+            {i.time.split(" ")[1].split(":")[0]} am
+          </p>
+          <img src={i.condition.icon} alt="" />
+          <p className="weatherCardText">{i.temp_c}°</p>
+        </div>
+      ))}
     </div>
   );
 }
