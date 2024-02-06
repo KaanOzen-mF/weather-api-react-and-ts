872d7f94e7dcbecb03ddabab1de1b937968bc279
KaanOzen-mF <kaanozn98@gmail.com>
2024-02-06 13:45:14 +0300

    display city weather done

---
+++ src/components/DisplayCityWeather.tsx
@@ -0,0 +1,10 @@
+import AddCityCard from "./AddCityCard";
+import { CityWeatherWrapper } from "./styles.module";
+
+export default function DisplayCityWeather() {
+  return (
+    <CityWeatherWrapper>
+      <AddCityCard />
+    </CityWeatherWrapper>
+  );
+}
