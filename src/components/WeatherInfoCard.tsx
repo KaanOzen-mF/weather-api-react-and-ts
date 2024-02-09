6f6efff53349b5cbb55036d86b3a85e2c7f2c1b7
KaanOzen-mF <kaanozn98@gmail.com>
2024-02-09 14:21:54 +0300

    fetch current weather function move utils

--- src/components/WeatherInfoCard.tsx
+++ src/components/WeatherInfoCard.tsx
@@ -1,7 +1,5 @@
-import { FaCloud } from "react-icons/fa";
 import { useState, useEffect } from "react";
-import axios from "axios";
-
+import { fetchCurrentWeather } from "../utils/fetchWeather";
 interface WeatherDataProps {
   location: {
     name: string;
@@ -31,25 +29,6 @@ interface WeatherDataProps {
 export default function WeatherInfoCard() {
   const [weatherData, setWeatherData] = useState<WeatherDataProps | null>(null);
 
-  const fetchCurrentWeather = async (query: string) => {
-    const options = {
-      method: "GET",
-      url: "https://weatherapi-com.p.rapidapi.com/forecast.json",
-      params: { query },
-      headers: {
-        "X-RapidAPI-Key": "b12886698dmsh2a39c211cc8b4aep12a465jsn115794388fb5",
-        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
-      },
-    };
-
-    try {
-      const response = await axios.request(options);
-      return response.data;
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       const { latitude, longitude } = position.coords;
@@ -63,8 +42,8 @@ export default function WeatherInfoCard() {
 
   return (
     <div className="weatherHourlyCardContainer">
-      {weatherData?.forecast.forecastday[0].hour.map((i) => (
-        <div className="weatherHourlyCard">
+      {weatherData?.forecast.forecastday[0].hour.map((i, k) => (
+        <div className="weatherHourlyCard" key={k}>
           <p className="weatherCardText">
             {i.time.split(" ")[1].split(":")[0]} am
           </p>
