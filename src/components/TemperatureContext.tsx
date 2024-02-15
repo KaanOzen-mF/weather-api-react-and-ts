11a196e54be2646fe180ab158f26c6b845595f42
KaanOzen-mF <kaanozn98@gmail.com>
2024-02-15 15:38:53 +0300

    temprature context provider added

---
+++ src/components/TemperatureContext.tsx
@@ -0,0 +1,39 @@
+// TemperatureContext.tsx
+import React, { createContext, useContext, useState, ReactNode } from "react";
+
+interface TemperatureContextType {
+  isCelsius: boolean;
+  toggleTemperature: () => void;
+}
+
+const TemperatureContext = createContext<TemperatureContextType | undefined>(
+  undefined
+);
+
+export const useTemperature = () => {
+  const context = useContext(TemperatureContext);
+  if (context === undefined) {
+    throw new Error("useTemperature must be used within a TemperatureProvider");
+  }
+  return context;
+};
+
+interface TemperatureProviderProps {
+  children: ReactNode;
+}
+
+export const TemperatureProvider: React.FC<TemperatureProviderProps> = ({
+  children,
+}) => {
+  const [isCelsius, setIsCelsius] = useState(true); // Başlangıç değeri Fahrenheit olarak ayarlandı.
+
+  const toggleTemperature = () => {
+    setIsCelsius(!isCelsius);
+  };
+
+  return (
+    <TemperatureContext.Provider value={{ isCelsius, toggleTemperature }}>
+      {children}
+    </TemperatureContext.Provider>
+  );
+};
