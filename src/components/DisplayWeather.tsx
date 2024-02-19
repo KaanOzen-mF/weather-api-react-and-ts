8259c5fe1aac023c56989645ec89fec5bbba9fb1
KaanOzen-mF <kaanozn98@gmail.com>
2024-02-19 17:57:05 +0300

    selected city props passed weather and weather info card components

--- src/components/DisplayWeather.tsx
+++ src/components/DisplayWeather.tsx
@@ -1,20 +1,22 @@
 import { WeatherWrapper } from "./styles.module";
 import { Weather } from "./Weather";
 import WeatherDate from "./WeatherDate";
-import WeatherInfoCard from "./WeatherInfoCard";
+import { WeatherInfoCard } from "./WeatherInfoCard";
 
 interface DisplayWeatherProps {
   setBackground: (background: string) => void;
+  selectedCity: string;
 }
 
 export const DisplayWeather: React.FC<DisplayWeatherProps> = ({
   setBackground,
+  selectedCity,
 }) => {
   return (
     <WeatherWrapper>
       <WeatherDate />
-      <Weather setBackground={setBackground} />
-      <WeatherInfoCard />
+      <Weather setBackground={setBackground} selectedCity={selectedCity} />
+      <WeatherInfoCard selectedCity={selectedCity} />
     </WeatherWrapper>
   );
 };
