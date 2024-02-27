2ff0eb760ddb2c71a480acc43437105002189214
KaanOzen-mF <kaanozn98@gmail.com>
2024-02-27 16:57:48 +0300

    DisplayWeather.tsx comments added

--- src/components/WeatherComponents/DisplayWeather.tsx
+++ src/components/WeatherComponents/DisplayWeather.tsx
@@ -3,19 +3,24 @@ import { Weather } from "./Weather";
 import WeatherDate from "./WeatherDate";
 import { WeatherInfoCard } from "./WeatherInfoCard";
 
+// Props definition for DisplayWeather component
 interface DisplayWeatherProps {
-  setBackground: (background: string) => void;
-  selectedCity: string;
+  setBackground: (background: string) => void; // Function to set the background based on weather conditions
+  selectedCity: string; // The city selected by the user
 }
 
+// DisplayWeather component to render weather-related components
 export const DisplayWeather: React.FC<DisplayWeatherProps> = ({
   setBackground,
   selectedCity,
 }) => {
   return (
     <WeatherWrapper>
+      {/* Component to display the current date */}
       <WeatherDate />
-      <Weather setBackground={setBackground} selectedCity={selectedCity} />
+      {/* Weather component that displays current weather data and updates the background */}
+      <Weather setBackground={setBackground} selectedCity={selectedCity} /> //
+      {/* Component to display detailed weather information for the selected city */}
       <WeatherInfoCard selectedCity={selectedCity} />
     </WeatherWrapper>
   );
