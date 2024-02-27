0831c75009c92f62bb9bc7df32874b2b2d36ef29
KaanOzen-mF <kaanozn98@gmail.com>
2024-02-27 17:07:50 +0300

    comment lines add | css convery style component

--- src/components/WeatherComponents/WeatherDate.tsx
+++ src/components/WeatherComponents/WeatherDate.tsx
@@ -1,11 +1,15 @@
-export default function WeatherDate() {
-  const dateObject = new Date();
+import { DateContainer, DateText } from "../styles.module";
 
+// WeatherDate component to display the current date
+export default function WeatherDate() {
+  const dateObject = new Date(); // Create a new date object to get the current date
+
+  // Convert the date to UTC string and then format it by taking only the first 4 parts (Day, Date, Month, Year)
   let date = dateObject.toUTCString();
   date = date.split(" ").slice(0, 4).join(" ");
   return (
-    <div className="dateContainer">
-      <p className="dateText">{date}</p>
-    </div>
+    <DateContainer>
+      <DateText>{date}</DateText>
+    </DateContainer>
   );
 }
