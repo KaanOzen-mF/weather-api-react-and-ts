1c5b660546886fae849ff60f1fddfefbdd485380
KaanOzen-mF <kaanozn98@gmail.com>
2024-02-27 14:05:41 +0300

    Search.tsx component convert to the style components | also comments added

--- src/components/WeatherNavbarComponents/SearchBar.tsx
+++ src/components/WeatherNavbarComponents/SearchBar.tsx
@@ -1,57 +1,81 @@
 import React, { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
-import { SearchBarWrapper } from "../styles.module";
+
+// Import styled components for styling the search bar elements
+import {
+  SearchBarInput,
+  SearchBarWrapper,
+  SearchBox,
+  SearchIcon,
+  SuggestionBox,
+  Suggestions,
+} from "../styles.module";
+
+// Import the utility function to fetch city suggestions based on user input
 import { fetchCitySuggestions } from "../../utils/fetchWeather";
 
+// Define the SearchBar functional component with a prop for handling city selection
 const SearchBar: React.FC<{ onCitySelect: (city: string) => void }> = ({
   onCitySelect,
 }) => {
+  // State for the search query input by the user
   const [searchQuery, setSearchQuery] = useState("");
+  // State for the list of suggestions returned from the API
   const [suggestions, setSuggestions] = useState<any[]>([]);
 
+  // UseEffect hook to handle side effects, in this case, fetching suggestions
   useEffect(() => {
+    // Function to load suggestions from the API
     const loadSuggestions = async () => {
       const newSuggestions = await fetchCitySuggestions(searchQuery);
+      // Update the suggestions state with the new suggestions
       setSuggestions(newSuggestions);
     };
-
+    // If the search query length is more than 2 characters, load suggestions
     if (searchQuery.length > 2) {
       loadSuggestions();
     } else {
+      // If the search query is less than 3 characters, clear suggestions
       setSuggestions([]);
     }
-  }, [searchQuery]);
+  }, [searchQuery]); // This effect depends on changes to searchQuery
 
+  // Function to handle click on a suggestion
   const handleSuggestionClick = (name: string) => {
+    // Set the search query to the selected suggestion's name
     setSearchQuery(name);
+    // Clear out the current suggestions
     setSuggestions([]);
+    // Call the onCitySelect prop with the selected city name
     onCitySelect(name);
   };
 
   return (
+    // Render the search bar with suggestions
     <>
       <SearchBarWrapper>
-        <div className="searchBox">
-          <FaSearch color="white" className="searchIcon" />
-          <input
+        <SearchBox>
+          <SearchIcon>
+            <FaSearch />
+          </SearchIcon>
+          <SearchBarInput
             placeholder="Search city"
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
-            className="searchBar"
           />
-        </div>
+        </SearchBox>
 
         {suggestions.length > 0 && (
-          <div className="suggestionBox">
+          <SuggestionBox>
             {suggestions.map((suggestion, index) => (
-              <div
+              <Suggestions
                 key={index}
                 onClick={() => handleSuggestionClick(suggestion.name)}
               >
                 {suggestion.name}, {suggestion.country}
-              </div>
+              </Suggestions>
             ))}
-          </div>
+          </SuggestionBox>
         )}
       </SearchBarWrapper>
     </>
--- src/components/styles.module.ts
+++ src/components/styles.module.ts
@@ -1,4 +1,132 @@
 import styled from "styled-components";
+import { FaSearch } from "react-icons/fa";
+const media = {
+  mobile: `(max-width: 600px)`,
+  tablet: `(min-width: 768px) and (max-width: 1024px)`,
+  laptop: `(min-width: 1024px) and (max-width: 1200px)`,
+  desktop: `(min-width: 1200px) and (max-width: 2000px)`,
+};
+
+export const SearchBarWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  border-radius: 24px;
+  backdrop-filter: blur(10px) saturate(130%);
+  background-color: rgba(98, 96, 96, 0.45);
+  width: fit-content;
+  padding: 6px;
+  align-items: center;
+  box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.75);
+  -webkit-box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.75);
+  z-index: 2;
+`;
+
+export const SearchBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const SearchIcon = styled(FaSearch)`
+  margin-left: 6px;
+  font-size: 24px;
+  color: white;
+
+  @media ${media.mobile} {
+    font-size: 16px;
+  }
+
+  @media ${media.tablet} {
+    font-size: 16px;
+  }
+
+  @media ${media.laptop} {
+    font-size: 16px;
+  }
+
+  @media ${media.desktop} {
+    font-size: 16px;
+  }
+`;
+
+export const SearchBarInput = styled.input`
+  border: none;
+  height: 48px;
+  margin-left: 16px;
+  background: none;
+  color: white;
+  outline: none;
+  width: 480px;
+  font-size: 24px;
+
+  @media ${media.mobile} {
+    height: 24px;
+    margin-left: 12px;
+    width: 142px;
+    font-size: 16px;
+  }
+
+  @media ${media.tablet} {
+    height: 24px;
+    margin-left: 12px;
+    width: 142px;
+    font-size: 16px;
+  }
+  @media ${media.laptop} {
+    height: 24px;
+    margin-left: 12px;
+    width: 142px;
+    font-size: 16px;
+  }
+  @media ${media.desktop} {
+    height: 36px;
+    margin-left: 12px;
+    width: 360px;
+    font-size: 24px;
+  }
+`;
+
+export const SuggestionBox = styled.div`
+  position: absolute;
+  top: 100%;
+  left: 0;
+  width: 496px;
+  cursor: pointer;
+  background-color: #8cd2ce;
+  border-radius: 24px;
+  padding: 6px;
+  font-size: 24px;
+
+  @media ${media.mobile} {
+    top: 100%;
+    width: 172px;
+    font-size: 16px;
+  }
+  @media ${media.tablet} {
+    top: 100%;
+    width: 172px;
+    font-size: 16px;
+  }
+  @media ${media.laptop} {
+    top: 100%;
+    width: 172px;
+    font-size: 16px;
+  }
+  @media ${media.desktop} {
+    top: 100%;
+    width: 400px;
+    font-size: 24px;
+  }
+`;
+
+export const Suggestions = styled.div`
+  &:hover {
+    border-radius: 24px;
+    color: white;
+  }
+`;
 
 export const WeatherWrapper = styled.div`
   display: flex;
@@ -11,7 +139,7 @@ export const WeatherWrapper = styled.div`
   box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.75);
   -webkit-box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.75);
   -moz-box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.75);
-  @media only screen and (max-width: 600px) {
+  @media ${media.mobile} {
     margin-right: 4%;
   }
 
@@ -49,22 +177,6 @@ export const NavbarWrapper = styled.div`
   @media only screen and (min-width: 1024px) and (max-width: 1200px) {
     margin-right: 2%;
   }
-`;
-
-export const SearchBarWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  position: relative;
-  border-radius: 24px;
-  backdrop-filter: blur(10px) saturate(130%);
-  background-color: rgba(98, 96, 96, 0.45);
-  width: fit-content;
-  padding: 6px;
-  align-items: center;
-  box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.75);
-  -webkit-box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.75);
-  -moz-box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.75);
-  z-index: 2;
 `;
 
 export const CityWeatherWrapper = styled.div`
