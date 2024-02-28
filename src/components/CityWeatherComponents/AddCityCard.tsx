7ae3136d5b43c998d72575fb1d9da9e53315ee7e
KaanOzen-mF <kaanozn98@gmail.com>
2024-02-28 10:32:05 +0300

    comment lines added | styled code implement

--- src/components/CityWeatherComponents/AddCityCard.tsx
+++ src/components/CityWeatherComponents/AddCityCard.tsx
@@ -1,11 +1,16 @@
+// Import styled components from styles.module
+import { AddCityCardWrapper, AddCityContent, PlusIcon } from "../styles.module";
+
+// Define AddCityCard component with a prop for handling click events
 const AddCityCard: React.FC<{ onAddClick: () => void }> = ({ onAddClick }) => {
   return (
-    <div className="add-city-card" onClick={onAddClick}>
-      <div className="add-city-content">
-        <span className="plus-icon">+</span>
+    // AddCityCardWrapper is a styled div that will trigger onAddClick when clicked
+    <AddCityCardWrapper onClick={onAddClick}>
+      <AddCityContent>
+        <PlusIcon>+</PlusIcon>
         <span>Add City</span>
-      </div>
-    </div>
+      </AddCityContent>
+    </AddCityCardWrapper>
   );
 };
 
