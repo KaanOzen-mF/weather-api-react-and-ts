9ff4d0c511c0b6cf140d10234d9dd89dcb39f8a8
KaanOzen-mF <kaanozn98@gmail.com>
2024-02-28 12:07:11 +0300

    comment lines change

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -8,35 +8,42 @@ import {
   ModalContent,
 } from "./styles.module";
 
+// Define the props for the Modal component
 type ModalProps = {
-  show: boolean;
-  onClose: () => void;
-  children: React.ReactNode;
-  onAddCity?: () => void;
+  show: boolean; // Whether the modal should be displayed
+  onClose: () => void; // Function to call when closing the modal
+  children: React.ReactNode; // Content to be displayed inside the modal
+  onAddCity?: () => void; // Optional function to call when adding a city
 };
 
+// Modal component definition
 const Modal: React.FC<ModalProps> = ({
   show,
   onClose,
   children,
   onAddCity,
 }) => {
+  // If the modal is not supposed to show, return null to render nothing
   if (!show) {
     return null;
   }
-
+  // Return the modal structure
   return (
     <ModalBackdrop>
+      {/* Modal backdrop for the overlay effect */}
       <ModalContent>
-        {children}
+        {/* Content container of the modal */}
+        {children} {/* Render the children elements passed to the modal */}
         <ModalButtonContainer>
-          {onAddCity && (
+          {/* Container for the modal action buttons */}
+          {onAddCity && ( // If the onAddCity function is provided, render the Add City button
             <AddCityButton onClick={onAddCity}>
-              <FaPlus />
+              <FaPlus /> {/* Add City button icon */}
             </AddCityButton>
           )}
           <CloseButton onClick={onClose}>
-            <FaX />
+            {/* Close button for the modal */}
+            <FaX /> {/* Close button icon */}
           </CloseButton>
         </ModalButtonContainer>
       </ModalContent>
