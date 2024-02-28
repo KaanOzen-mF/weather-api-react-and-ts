ffaf92def8aaacaffd4510980bcd7d631d8f0a7b
KaanOzen-mF <kaanozn98@gmail.com>
2024-02-28 10:19:44 +0300

    comment lines add

--- src/components/CityWeatherComponents/DisplayCityWeather.tsx
+++ src/components/CityWeatherComponents/DisplayCityWeather.tsx
@@ -1,7 +1,10 @@
+// Importing the CityWeather component and styled component for layout
 import CityWeather from "./CityWeather";
 import { CityWeatherWrapper } from "../styles.module";
 
+// The DisplayCityWeather functional component acts as a container for CityWeather
 export default function DisplayCityWeather() {
+  // Rendering the CityWeather component within a styled wrapper for consistent styling
   return (
     <CityWeatherWrapper>
       <CityWeather />
